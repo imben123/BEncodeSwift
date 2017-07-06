7e104ab9120dbfb59e0314d4ccd0d67e7ff7603b
Ben Davis <imben123@gmail.com>
2017-07-06 18:31:27 +0200

    Added methods to convert UInt16, UInt32 and UInt64 between binary
    This includes a default of big endian byte order, with the option to choose little endian

--- BEncode/Bytes.swift
+++ BEncode/Bytes.swift
@@ -14,6 +14,21 @@ public extension Data {
     func toUInt8() -> UInt8 {
         return self[0]
     }
+    
+    func toUInt16(bigEndian: Bool = true) -> UInt16 {
+        let result: UInt16 = self.withUnsafeBytes { $0.pointee }
+        return bigEndian ? result.bigEndian : result
+    }
+    
+    func toUInt32(bigEndian: Bool = true) -> UInt32 {
+        let result: UInt32 = self.withUnsafeBytes { $0.pointee }
+        return bigEndian ? result.bigEndian : result
+    }
+    
+    func toUInt64(bigEndian: Bool = true) -> UInt64 {
+        let result: UInt64 = self.withUnsafeBytes { $0.pointee }
+        return bigEndian ? result.bigEndian : result
+    }
 }
 
 public extension UInt8 {
@@ -24,5 +39,44 @@ public extension UInt8 {
     
     init(data: Data) {
         self = data.toUInt8()
+    }
+}
+
+public extension UInt16 {
+    
+    func toData(bigEndian: Bool = true) -> Data {
+        var copy = bigEndian ? self.bigEndian : self
+        let pointer = withUnsafeBytes(of: &copy) { return $0.baseAddress }
+        return Data(bytes: pointer!, count: 2)
+    }
+    
+    init(data: Data, bigEndian: Bool = true) {
+        self = data.toUInt16()
+    }
+}
+
+public extension UInt32 {
+    
+    func toData(bigEndian: Bool = true) -> Data {
+        var copy = bigEndian ? self.bigEndian : self
+        let pointer = withUnsafeBytes(of: &copy) { return $0.baseAddress }
+        return Data(bytes: pointer!, count: 4)
+    }
+    
+    init(data: Data, bigEndian: Bool = true) {
+        self = data.toUInt32()
+    }
+}
+
+public extension UInt64 {
+    
+    func toData(bigEndian: Bool = true) -> Data {
+        var copy = bigEndian ? self.bigEndian : self
+        let pointer = withUnsafeBytes(of: &copy) { return $0.baseAddress }
+        return Data(bytes: pointer!, count: 8)
+    }
+    
+    init(data: Data, bigEndian: Bool = true) {
+        self = data.toUInt64()
     }
 }
--- BEncode/BytesTests.swift
+++ BEncode/BytesTests.swift
@@ -27,18 +27,121 @@ class BytesTests: XCTestCase {
         
         let pointer = (data as NSData).bytes.bindMemory(to: Byte.self, capacity: data.count)
         let value: UInt8 = pointer.pointee
+        XCTAssertEqual(data.count, 1)
+        XCTAssertEqual(value, integer)
+    }
+    
+    func testEncode16BitIntegerBigEndian() {
+        let integer: UInt16 = 12345
+        let data = integer.toData()
+        
+        let pointer = (data as NSData).bytes.bindMemory(to: UInt16.self, capacity: data.count)
+        let value: UInt16 = pointer.pointee.bigEndian
+        XCTAssertEqual(data.count, 2)
+        XCTAssertEqual(value, integer)
+    }
+    
+    func testEncode16BitIntegerLittleEndian() {
+        let integer: UInt16 = 12345
+        let data = integer.toData(bigEndian: false)
+        
+        let pointer = (data as NSData).bytes.bindMemory(to: UInt16.self, capacity: data.count)
+        let value: UInt16 = pointer.pointee
+        XCTAssertEqual(data.count, 2)
+        XCTAssertEqual(value, integer)
+    }
+    
+    func testEncode32BitIntegerBigEndian() {
+        let integer: UInt32 = 1234567891
+        let data = integer.toData()
+        
+        let pointer = (data as NSData).bytes.bindMemory(to: UInt32.self, capacity: data.count)
+        let value: UInt32 = pointer.pointee.bigEndian
+        XCTAssertEqual(data.count, 4)
+        XCTAssertEqual(value, integer)
+    }
+    
+    func testEncode32BitIntegerLittleEndian() {
+        let integer: UInt32 = 1234567891
+        let data = integer.toData(bigEndian: false)
+        
+        let pointer = (data as NSData).bytes.bindMemory(to: UInt32.self, capacity: data.count)
+        let value: UInt32 = pointer.pointee
+        XCTAssertEqual(data.count, 4)
+        XCTAssertEqual(value, integer)
+    }
+    
+    func testEncode64BitIntegerBigEndian() {
+        let integer: UInt64 = 12345678912345678912
+        let data = integer.toData()
+        
+        let pointer = (data as NSData).bytes.bindMemory(to: UInt64.self, capacity: data.count)
+        let value: UInt64 = pointer.pointee.bigEndian
+        XCTAssertEqual(data.count, 8)
+        XCTAssertEqual(value, integer)
+    }
+    
+    func testEncode64BitIntegerLittleEndian() {
+        let integer: UInt64 = 12345678912345678912
+        let data = integer.toData(bigEndian: false)
+        
+        let pointer = (data as NSData).bytes.bindMemory(to: UInt64.self, capacity: data.count)
+        let value: UInt64 = pointer.pointee
+        XCTAssertEqual(data.count, 8)
         XCTAssertEqual(value, integer)
     }
     
     func testIntegerFromData() {
-        doTestForIntegerFromData(0)
-        doTestForIntegerFromData(5)
-        doTestForIntegerFromData(UInt8.max)
+        doTestFor8BitIntFromData(0)
+        doTestFor8BitIntFromData(5)
+        doTestFor8BitIntFromData(UInt8.max)
+        
+        doTestFor16BitIntFromData(0)
+        doTestFor16BitIntFromData(5)
+        doTestFor16BitIntFromData(UInt16.max)
+        
+        doTestFor32BitIntFromData(0)
+        doTestFor32BitIntFromData(5)
+        doTestFor32BitIntFromData(UInt32.max)
+        
+        doTestFor64BitIntFromData(0)
+        doTestFor64BitIntFromData(5)
+        doTestFor64BitIntFromData(UInt64.max)
     }
     
-    func doTestForIntegerFromData(_ integer: UInt8) {
+    func doTestFor8BitIntFromData(_ integer: UInt8) {
         let data = integer.toData()
         let result = data.toUInt8()
         XCTAssertEqual(result, integer)
+    }
+    
+    func doTestFor16BitIntFromData(_ integer: UInt16) {
+        let data = integer.toData()
+        let result = data.toUInt16()
+        XCTAssertEqual(result, integer)
+        
+        let dataLittleEndian = integer.toData(bigEndian: false)
+        let resultLittleEndian = dataLittleEndian.toUInt16(bigEndian: false)
+        XCTAssertEqual(resultLittleEndian, integer)
+    }
+    
+    func doTestFor32BitIntFromData(_ integer: UInt32) {
+        let data = integer.toData()
+        let result = data.toUInt32()
+        XCTAssertEqual(result, integer)
+        
+        let dataLittleEndian = integer.toData(bigEndian: false)
+        let resultLittleEndian = dataLittleEndian.toUInt32(bigEndian: false)
+        XCTAssertEqual(resultLittleEndian, integer)
+    }
+    
+    func doTestFor64BitIntFromData(_ integer: UInt64) {
+        let data = integer.toData()
+        let result = data.toUInt64()
+        XCTAssertEqual(result, integer)
+        
+        let dataLittleEndian = integer.toData(bigEndian: false)
+        let resultLittleEndian = dataLittleEndian.toUInt64(bigEndian: false)
+        XCTAssertEqual(resultLittleEndian, integer)
     }
 }
